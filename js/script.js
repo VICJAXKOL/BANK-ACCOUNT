2cf6f920363e1918f6b24edddf7842e1c4db4865
Victor Thompson <kelechithompson26@gmail.com>
2023-02-16 11:46:00 +0100

    add bank assignID and addAccount prototype

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function bankAccount() {
   this.accounts = {};
   this.currentId = 2209146835;
 }
+bankAccount.prototype.assignID = function () {
+  this.currentId += 1260;
+  return this.currentId;
+};
+bankAccount.prototype.addAccount = function (account) {
+  account.id = this.assignID();
+  this.accounts[account.id] = account
+}
